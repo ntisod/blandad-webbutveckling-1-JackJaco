e66f5829d61e6947b719e0d8e4e178f91a140ee8
JackJaco <jacob.haara@elev.ga.ntig.se>
2021-04-19 12:58:08 +0200

    tagit bort .html i filnamn

---
+++ javascript-exempel.js
@@ -0,0 +1,8 @@
+function authorDialog(){
+    alert("Jack har skapat den här sidan. Den 03/23/21");
+}
+
+function umuk(){
+    document.getElementById("rubrik").innerHTML = "Jack";
+    document.getElementById("text").style.backgroundColor = "Cyan";
+}
